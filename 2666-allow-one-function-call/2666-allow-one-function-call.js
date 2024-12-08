4a36dce34981db6bbdccbfc57bcbeeaec2082ffb
NURIHO <97593669+nuriring@users.noreply.github.com>
2024-12-08 23:39:45 +0900

    Time: 62 ms (8.68%), Space: 49.2 MB (18.04%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let chance = 0
+    return function(...args){
+        console.log(args, 'args')
+        // return fn(args)
+        if (chance === 1) {
+            return undefined
+        } else {
+            chance++
+            return fn(...args)
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
