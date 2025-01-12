8db63aeb20bb2a36b483e74de68f5bb9c6ed6931
NURIHO <97593669+nuriring@users.noreply.github.com>
2025-01-12 21:12:52 +0900

    Time: 63 ms (38.42%), Space: 50 MB (5.43%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -5,15 +5,24 @@
  */
 var timeLimit = function(fn, t) {
     return async function(...args) {
-        // 시간 초과 Promise 생성
-        const timeoutPromise = new Promise((_, reject) => {
-            setTimeout(() => {
-                reject("Time Limit Exceeded");
-            }, t);
-        });
         
-        // fn 실행 Promise와 시간 초과 Promise를 경쟁
-        return Promise.race([fn(...args), timeoutPromise]);
+        // // 시간 초과를 나타내는 Promise 생성
+        // const timeoutPromise = new Promise((_, reject) => {
+        //     setTimeout(() => {
+        //         reject("Time Limit Exceeded");
+        //     }, t);
+        // });
+        
+        // // 주어진 함수 실행과 시간 초과 Promise를 경쟁시킴
+        // return Promise.race([fn(...args), timeoutPromise]);
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject("Time Limit Exceeded")
+            }, t)
+            fn(...args).then(resolve).catch(reject)
+        })
+
     };
 };
 /**
