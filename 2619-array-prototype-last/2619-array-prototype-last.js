fbe099598c02da488742d355964dbd634e810b07
NURIHO <97593669+nuriring@users.noreply.github.com>
2025-02-09 20:59:19 +0900

    Time: 51 ms (53.59%), Space: 49.2 MB (8.59%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,16 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    console.log(this, 'this')
+    if (!this.length) {
+        return -1
+    }
+
+    return this[this.length-1]
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
