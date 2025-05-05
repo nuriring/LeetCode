3002dacb471100fecd66ff9e6a10d6d3e984e881
NURIHO <97593669+nuriring@users.noreply.github.com>
2025-05-05 18:02:15 +0900

    Time: 390 ms (9.08%), Space: 108.4 MB (8.95%) - LeetHub

---
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+//  id가 같으면 arr2가 override
+var join = function(arr1, arr2) {
+  
+
+  const merged = [...arr1, ...arr2].reduce((acc, item) => {
+  const existing = acc.get(item.id) || {};
+  acc.set(item.id, { ...existing, ...item });
+  return acc;
+}, new Map());
+
+
+//   const res = Array.from(merged.values())
+  return Array.from(merged.values()).sort((a, b) => a.id - b.id);
+
+//   return res
+};
